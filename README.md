acabd140c94bfa67ec8b4f0846d39435c8a9d272
Mark C <mc@blaidd.cymru>
2018-02-02 14:54:57 +0000

    updated

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Pub Name Generator
 
+## Make your own British Pub name!
+
+Data: [http://archive.org/stream/pubscollectionof00tomluoft#page/48/mode/thumb](http://archive.org/stream/pubscollectionof00tomluoft#page/48/mode/thumb)
+
+This uses a very simple Markov Chain function to generate a new pub name from the 2794 currently listed in the file `pub-list.txt` extracted from the web page above! :-P
+
 
---
+++ pubgen.py
@@ -0,0 +1,43 @@
+import random
+
+def read_pubs(filename):
+    with open(filename, 'r') as file:
+        contents = file.read().replace('The','').replace('\n\n',' ').replace('\n',' ') # remove all line breaks
+                     # remove all the 'The's... Add them back in later
+    return contents
+
+def build_Markov_chain(in_data, chain = {}):
+    words = in_data.split(' ')
+    index = 1
+    for word in words[index:]:
+        key = words[index-1]
+        if key in chain:
+            chain[key].append(word)
+        else:
+            chain[key] = [word]
+        index += 1
+    return chain
+
+def generate_pub_name(chain, leng=random.choice([2,3,4])):
+    w1 = random.choice(list(chain.keys())).capitalize() # get a word, add a capital letter
+    text = w1
+    while len(text.split(' ')) < leng:
+        w2 = random.choice(chain[w1])
+        w1 = w2
+        text += ' '+ w2
+    pubname = "The "+text
+    if pubname.endswith("and"):
+        pubname += " "
+        pubname += random.choice(chain["and"])
+    return pubname
+
+if __name__ == "__main__":
+    print "[i] loading data..."
+    fileread = read_pubs("pub-list.txt")
+    print "[*] generating chain..."
+    chain = build_Markov_chain(fileread)
+    print "[i] Done! Here are some random names:"
+    for i in range(1,10):
+        print generate_pub_name(chain)
+
+
